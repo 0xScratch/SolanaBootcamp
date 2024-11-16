8bd97d01fc1fab31960cec336e07d8a201e884ff
0xScratch <aryanhunny98@gmail.com>
2024-11-16 20:55:00 +0530

    Completed Homework 5

--- homeworks_rust/homeworks/homework5/if/if1.rs
+++ homeworks_rust/homeworks/homework5/if/if1.rs
@@ -9,6 +9,11 @@ pub fn bigger(a: i32, b: i32) -> i32 {
     // - additional variables
     // Execute `rustlings hint if1` for hints
 
+    if a > b {
+        return a
+    } else {
+        return b
+    }
 }
 
 // Don't mind this for now :)
